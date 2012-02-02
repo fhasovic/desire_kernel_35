e73c60f3dd1b956f1d23a864a5b699985dc80394
Fikret Hasovic <fikret@ubuntu.(none)>
2012-02-02 10:54:51 +0100

    Set built-in flag for wifi (ACA)

--- arch/arm/mach-msm/board-htcleo-mmc.c
+++ arch/arm/mach-msm/board-htcleo-mmc.c
@@ -210,6 +210,7 @@ static unsigned int htcleo_wifi_status(struct device *dev)
 
 static struct mmc_platform_data htcleo_wifi_data = {
 	.ocr_mask		= MMC_VDD_26_27,  //lowered to match voltage given by Marc1706
+	.built_in    		= 1,
 	.status			= htcleo_wifi_status,
 	.register_status_notify	= htcleo_wifi_status_register,
 	.embedded_sdio		= &htcleo_wifi_emb_data,
