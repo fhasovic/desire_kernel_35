3481391a584fcbe0b32fd77d892cb4e2e7cf3a0a
Fikret Hasovic <fikret@ubuntu.(none)>
2012-02-08 13:51:22 +0100

    [ARM] msm8k: fixes for audio_ctl and pcm_out
    -- q6_ioctl was not doing proper checks on user data
    -- pcm_ioctl would crash if AUDIO_SET_VOLUME got called before AUDIO_START

--- arch/arm/mach-msm/qdsp6_1550/audio_ctl.c
+++ arch/arm/mach-msm/qdsp6_1550/audio_ctl.c
@@ -133,8 +133,7 @@ static int q6_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static int q6_ioctl(struct inode *inode, struct file *file,
-		unsigned int cmd, unsigned long arg)
+static long q6_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	int rc;
 	uint32_t n;
@@ -147,28 +146,40 @@ static int q6_ioctl(struct inode *inode, struct file *file,
 	case AUDIO_SWITCH_DEVICE:
 		rc = copy_from_user(&id, (void *)arg, sizeof(id));
 		AUDIO_INFO("SWITCH DEVICE %d, acdb %d\n", id[0], id[1]);
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_do_routing(id[0], id[1]);
 		break;
 	case AUDIO_SET_VOLUME:
 		rc = copy_from_user(&n, (void *)arg, sizeof(n));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_set_rx_volume(n);
 		break;
 	case AUDIO_SET_MUTE:
 		rc = copy_from_user(&n, (void *)arg, sizeof(n));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_set_tx_mute(n);
 		break;
 	case AUDIO_UPDATE_ACDB:
 		rc = copy_from_user(&id, (void *)arg, sizeof(id));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_update_acdb(id[0], id[1]);
 		break;
 	case AUDIO_START_VOICE:
-		if (arg == 0) {
+		if (arg == 0)
 			id[0] = id[1] = 0;
-		} else if (copy_from_user(&id, (void*) arg, sizeof(id))) {
+		else if (copy_from_user(&id, (void*) arg, sizeof(id))) {
 			pr_info("voice: copy acdb_id from user failed\n");
 			rc = -EFAULT;
 			break;
@@ -190,7 +201,10 @@ static int q6_ioctl(struct inode *inode, struct file *file,
 		break;
 	case AUDIO_REINIT_ACDB:
 		rc = copy_from_user(&filename, (void *)arg, sizeof(filename));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_reinit_acdb(filename);
 		break;
 	case AUDIO_ENABLE_AUXPGA_LOOPBACK: {
@@ -234,7 +248,7 @@ static int q6_release(struct inode *inode, struct file *file)
 static struct file_operations q6_dev_fops = {
 	.owner		= THIS_MODULE,
 	.open		= q6_open,
-	.ioctl		= q6_ioctl,
+	.unlocked_ioctl    = q6_ioctl,
 	.release	= q6_release,
 };
 
